95ff6aab073d63df0334552b80c79dbff2b036f1
Aceade <philiprowlands90@gmail.com>
2016-12-04 14:16:51 +0000

    Initial commit: README, base script, ignore file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Unity-Multi-Standalone-Builder
 A Unity Editor script that allows you to build standalone builds for multiple platforms with one click.
+
+Creating standalone builds for Linux, Mac and Windows requires manually changing the build platform after each build. This is tedious, so I created this Editor script to automate the process. 
+I created this with Unity 5.3.6f1. The API may be different in 5.4 or 5.5.
+
+## How to use it
+Download or clone the repo, and copy it into the Assets/Aceade folder.
+Click on the Tools menu, then click on "Aceade", then "Multi-Standalone Build". By default, it creates a 64-bit Linux, 64-bit Windows and 64-bit Mac build in a folder named "Player Files" directly below the project root.
+
+### Changing the defaults
+The default naming convention is "{Application.name}_{Platform}_{BuildDate}", e.g. "BuildProcess_Linux_4.12.2016". To change the build date, you will need to edit the `GetBuildDate` method.
+
+## Licence
+
+MIT Licence
---
+++ Scripts/Editor/MultiStandaloneBuilder.cs
@@ -0,0 +1,94 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+public class MultiStandaloneBuilder {
+
+    // constants for defaults
+    const string WINDOWS_FILENAME = "_Windows";
+    const string LINUX_FILENAME = "_Linux";
+    const string MAC_FILENAME = "_Mac";
+    const string SCENE_FOLDER = "Assets/_Scenes/";
+    const string BUILD_PATH = "Player Files/";
+
+    // TODO: find how to combine these
+    static BuildOptions devOptions = BuildOptions.AllowDebugging;
+
+    [MenuItem("Tools/Aceade/Multi-Standalone Build")]
+    public static void Build()
+    {
+        Debug.LogFormat("Creating new builds at {0}", DateTime.Now);
+
+        // Assign your levels here.
+        // TODO: prompt for list of levels
+        var levels = new string[]{
+            SCENE_FOLDER + "Sample Scene.unity"
+        };
+
+        // prompt for the build path. Defaults to using the product name.
+        var savePath = EditorUtility.SaveFolderPanel("Put build in...", BUILD_PATH, Application.productName);
+
+        // get the build date, then kick off the builds.
+        var buildDate = GetBuildDate();
+        MakeLinuxBuild(levels, buildDate, savePath);
+        MakeWindowsBuild(levels, buildDate, savePath);
+        MakeMacBuild(levels, buildDate, savePath);
+
+        Debug.LogFormat("All builds finished at {0}", DateTime.Now);
+    }
+
+    /// <summary>
+    /// Create a string representing the build date. The default syntax is "_dd.mm.yyyy", e.g "_04.12.2016".
+    /// </summary>
+    /// <returns>The build date.</returns>
+    static string GetBuildDate()
+    {
+        // calculate the build date
+        DateTime date = DateTime.Now;
+        string buildDate = string.Format("_{0}.{1}.{2}",date.Day, date.Month, date.Year);
+        Debug.LogFormat("Creating new builds on {0}", buildDate);
+        return buildDate;
+    }
+
+    /// <summary>
+    /// Makes a Linux build.
+    /// </summary>
+    /// <param name="levelsToBuild">Levels to build.</param>
+    /// <param name="dateString">Date string.</param>
+    /// <param name="path">Path.</param>
+	static void MakeLinuxBuild(string[] levelsToBuild, string dateString, string path)
+    {
+        Debug.Log("Creating Linux build");
+        string fileName = LINUX_FILENAME + dateString + ".x86_64";
+        BuildTarget target = BuildTarget.StandaloneLinux64;
+        BuildPipeline.BuildPlayer(levelsToBuild, path + fileName, target, devOptions);
+    }
+
+    /// <summary>
+    /// Makes the Windows build.
+    /// </summary>
+    /// <param name="levelsToBuild">Levels to build.</param>
+    /// <param name="dateString">Date string.</param>
+    /// <param name="path">Path.</param>
+    static void MakeWindowsBuild(string[] levelsToBuild, string dateString, string path)
+    {
+        Debug.Log("Creating Windows build");
+        string fileName = WINDOWS_FILENAME + dateString + ".exe";
+        BuildTarget target = BuildTarget.StandaloneWindows64;
+        BuildPipeline.BuildPlayer(levelsToBuild, path + fileName, target, devOptions);
+    }
+
+    /// <summary>
+    /// Makes a build for Mac devices.
+    /// </summary>
+    /// <param name="levelsToBuild">Levels to build.</param>
+    /// <param name="dateString">Date string.</param>
+    /// <param name="path">Path.</param>
+    static void MakeMacBuild(string[] levelsToBuild, string dateString, string path)
+    {
+        Debug.Log("Creating Mac OSX build");
+        string fileName = MAC_FILENAME + dateString + ".app";
+        BuildTarget target = BuildTarget.StandaloneOSXIntel64;
+        BuildPipeline.BuildPlayer(levelsToBuild, path + fileName, target, devOptions);
+    }
+}
